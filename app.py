2c4ab53347c825e3beb1c0e959d5bee3a44fceed
Noorthistime <noorsayyed.atwork@gmail.com>
2026-08-30 14:42:30 +0530

    Fixing the navigation panel error!

--- app.py
+++ app.py
@@ -344,7 +344,8 @@ if st.session_state.theme == 'default':
     }
 
     /* The Trojan Horse Strategy: Hide Streamlit's native dot container */
-    [data-testid="stSidebar"] [data-testid="stRadio"] > div[role="radiogroup"] > label > div:first-of-type {
+    [data-testid="stSidebar"] [data-testid="stRadio"] > div[role="radiogroup"] > label > div:not([data-testid="stMarkdownContainer"]),
+    [data-testid="stSidebar"] [data-testid="stRadio"] > div[role="radiogroup"] > label > svg {
         display: none !important;
     }
 
@@ -724,7 +725,8 @@ elif st.session_state.theme == 'stitch':
     }
 
     /* The Trojan Horse Strategy: Hide Streamlit's native dot container */
-    [data-testid="stSidebar"] [data-testid="stRadio"] > div[role="radiogroup"] > label > div:first-of-type {
+    [data-testid="stSidebar"] [data-testid="stRadio"] > div[role="radiogroup"] > label > div:not([data-testid="stMarkdownContainer"]),
+    [data-testid="stSidebar"] [data-testid="stRadio"] > div[role="radiogroup"] > label > svg {
         display: none !important;
     }
 
